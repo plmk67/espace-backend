b0201510381511a6dafa0f95e4154c5797ce8fce
Vincent <Yip.vincent@gmail.com>
2022-11-22 20:19:32 -0500

    fixes CORS error for API call

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(bodyParser.json());
 
 //cors-policy error handling
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", "*");
 
   res.setHeader(
     "Access-Control-Allow-Headers",
